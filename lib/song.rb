070f59ab9bf45a5b100ad1a9403ae8f962eb82e1
maiya777 <maiya777@list.ru>
2015-11-17 16:52:36 +0300

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,12 @@
 class Song
-  # code goes here
+  
+  attr_accessor :artist, :title
+  
+  def slugify
+    song_title = self.title.gsub(" ", "_").downcase
+    file = Tempfile.new([song_title, ".txt"], "tmp")
+
+    file.write("#{self.artist.name} - #{self.title}")
+    file.close
+  end
 end
